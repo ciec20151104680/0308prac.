4f05d7e1ab6c9fc6573cdd13910bed21b7b11a8a
ciec20151104680 <cxxxe4680@Mac28.local>
2017-03-20 10:48:55 +0800

    update.

--- 0308prac./ViewController.swift
+++ 0308prac./ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var a: UITextField!
-    @IBOutlet weak var b: UITextField!
-    @IBOutlet weak var c: UITextField!
-    @IBAction func add(_ sender: Any) {
-        var temp : Int
-        temp = Int(a.text!)!+Int(b.text!)!
-        c.text="\(temp)"
-        
-    }
+    var a1:String = ""
+    var a2:String = ""
+    var a3:String = ""
+    
+    @IBOutlet weak var a:UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,6 +24,70 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func `as`(_ sender: UIButton) {
+        
+        let q = sender.currentTitle
+        
+       if q=="+"||q=="-"||q=="*"||q=="/"
+        
+       {
+            a3 = q!
+            return
+        }
+        else if q == "=" || q == "c"
+        {
+            var r:Double = 0
+            let s:Int = 0
+            switch a3 {
+            case "+":
+                    r = Double(a1)! + Double(a2)!
+                    a.text = "\(r)"
+                    a1=String（r）
+                    a2=""
+                    a3=""
+            case "-":
+                r = Double(a1)! + Double(a2)!
+                a.text = "\(r)"
+                a1=String（r）
+                a2=""
+                a3=""
+            case "*":
+                r = Double(a1)! + Double(a2)!
+                a.text = "\(r)"
+                a1=String（r）
+                a2=""
+                a3=""
+            case "/":
+                r = Double(a1)! + Double(a2)!
+                a.text = "\(r)"
+                a1=String（r）
+                a2=""
+                a3=""
+            default:
+                a1=""
+                a2=""
+                a3=""
+                a.text = "\(s)"
+            }
+            return
+            
+        }
+        
+        if a3 == ""
+        {
+           a1=a1+q!
+           a.text=a1
+        }
+        else
+        {
+           a2=a2+q!
+           a.text=a2
+        }
+        
     }
 
 
